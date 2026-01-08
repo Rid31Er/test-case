1012ff0501341662cab25f7c5b78b9df7170cf68
ridhwan <ridhwan.erlangga@roketin.com>
2026-01-09 06:44:19 +0700

    add src app

---
+++ src/config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=' . getenv('DB_HOST') . ';port=' . getenv('DB_PORT') . ';dbname=' . getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'charset' => 'utf8',
+];
+
